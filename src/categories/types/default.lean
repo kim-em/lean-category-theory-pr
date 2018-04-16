e4e436d784c042fb64c51a794a6db26f96370d3d
Scott Morrison <scott.morrison@anu.edu.au>
2018-04-16 13:39:14 +1000

    minimising

--- src/categories/types/default.lean
+++ src/categories/types/default.lean
@@ -60,29 +60,4 @@ definition UniverseLift : Functor (Type u) (Type (u+1)) :=
 { onObjects := λ X, ulift.{u+1} X,
   onMorphisms := λ X Y f, λ x : ulift.{u+1} X, ulift.up (f x.down) }
 
-definition Bijection (α β : Type u) := Isomorphism α β 
-
-@[simp] definition Bijection.witness_1 {α β : Type u} (iso : Bijection α β) (x : α) : iso.inverse (iso.morphism x) = x :=
-begin
-  have p := iso.witness_1_lemma, unfold_projs at p,
-  exact congr_fun p x,
-end
-@[simp] definition Bijection.witness_2 {α β : Type u} (iso : Bijection α β) (x : β) : iso.morphism (iso.inverse x) = x :=
-begin
-  have p := iso.witness_2_lemma, unfold_projs at p,
-  exact congr_fun p x,
-end
-
--- TODO the @s are unpleasant here (ask for help during PR)
-@[simp] definition is_Isomorphism_in_Types.witness_1 {α β : Type u} (f : α → β) (h : @is_Isomorphism _ _ α β f) (x : α) : h.inverse (f x) = x :=
-begin
-  have p := h.witness_1, unfold_projs at p,
-  exact congr_fun p x,
-end
-@[simp] definition is_Isomorphism_in_Types.witness_2 {α β : Type u} (f : α → β) (h : @is_Isomorphism _ _ α β f) (x : β) : f (h.inverse x) = x :=
-begin
-  have p := h.witness_2, unfold_projs at p,
-  exact congr_fun p x,
-end
-
 end categories.types
