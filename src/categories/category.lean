c7195a2827cbf714a8ba9c0cb2513d8473bad48e
Scott Morrison <scott@tqft.net>
2018-04-29 23:28:32 +1000

    ?

--- src/categories/category.lean
+++ src/categories/category.lean
@@ -56,30 +56,4 @@ instance category.has_one : has_one (X ⟶ X) :=
 @[simp] def category.left_identity_lemma' (f : X ⟶ Y) : 1 ≫ f = f := begin unfold has_one.one, simp end
 @[simp] def category.right_identity_lemma' (f : X ⟶ Y) : f ≫ 1 = f := begin unfold has_one.one, simp end
 
-class small (C : Type (u+1)) :=
-(model : Type u)
-(smallness : equiv C model)
-
-instance (α : Type u) : small (ulift.{u+1 u} α) := 
-{ model := α, 
-  smallness := { to_fun := ulift.down,
-                 inv_fun := ulift.up,
-                 left_inv := sorry,
-                 right_inv := sorry } }
-
-class small_category {C : Type (u+1)} extends category C, small C.
-
--- structure small (α : Type u) : Type (u + 1) :=
--- up :: (down : α)
-
--- namespace small
--- /- Bijection between α and ulift.{v} α -/
--- @[simp] lemma up_down {α : Type u} : ∀ (b : small.{u} α), up (down b) = b
--- | (up a) := rfl
-
--- @[simp] lemma down_up {α : Type u} (a : α) : down (up.{u} a) = a := rfl
--- end small
-
--- notation a `⟶ₛ` b := category.Hom (small.up a) (small.up b)
-
 end categories
