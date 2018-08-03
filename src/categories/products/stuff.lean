93e20f31425d946a6a9b767166c815810199f868
Scott Morrison <scott.morrison@anu.edu.au>
2018-08-03 18:54:34 +1000

    stuff

--- src/categories/products/stuff.lean
+++ src/categories/products/stuff.lean
@@ -20,23 +20,31 @@ end
 
 open tactic
 
-meta def mk_congr_arg_using_dsimp (G W : expr) (F : name) : tactic unit := 
--- I have no idea how to achieve this: this doesn't work, but is my best guess.
+meta def local_def_value (e : expr) : tactic expr := do
+do (v,_) ← solve_aux `(true) (do
+         lc ← local_context,
+         let e := lc.reverse.head,
+         (expr.elet n t v _) ← (revert e >> target)
+           | fail format!"{e} is not a local definition",
+         return v),
+   return v
+
+-- Sometimes `mk_congr_arg` fails, when the function is 'superficially dependent'.
+-- This hack first dsimplifies the function before building the `congr_arg` expression.
+-- Unfortunately it creates a dummy hypothesis that I can't work out how to avoid or dispose of cleanly.
+meta def mk_congr_arg_using_dsimp (G W : expr) (u : list name) : tactic expr := 
 do
   s ← simp_lemmas.mk_default,
   t ← infer_type G,
-  t' ← s.dsimplify [F] t {fail_if_unchanged := ff},
-
-  tactic.definev `g t' G,
-
-  ca ← to_expr ```(congr_arg (g : %%t') %%W),
-  cat ← infer_type ca,
-  he ← tactic.definev `h cat ca,
-  `[dsimp [g] at h]
+  t' ← s.dsimplify u t {fail_if_unchanged := ff},
+  aux ← tactic.definev `_mk_congr_arg_aux t' G,
+  ca ← to_expr ```(congr_arg _mk_congr_arg_aux %%W),
+  ca_t ← infer_type ca,
+  s.dsimplify [`_mk_congr_arg_aux] ca {fail_if_unchanged := ff}
   
 lemma test2 (m n : ℕ) (w : m = n) : G n == G m :=
 begin
   -- tactic.get_local `w >>= λ W, mk_congr_arg_using_dsimp `(G) W `F >>= tactic.trace,
-  ((tactic.get_local `w) >>= λ W, mk_congr_arg_using_dsimp `(G) W `F),
+  let h := by ((tactic.get_local `w) >>= λ W, mk_congr_arg_using_dsimp `(G) W [`F]) >>= exact,
   rw h,
 end
